d7c081c8da4a13e56569a3b6df63cc369bb605db
Scott Weber <scott.d.weber@gmail.com>
2020-03-24 22:24:31 -0400

    change Person to Kotlin

--- app/src/main/java/com/proquest/interview/phonebook/Person.java
+++
@@ -1,7 +0,0 @@
-package com.proquest.interview.phonebook;
-
-public class Person {
-	public String name;
-	public String phoneNumber;
-	public String address;
-}
---
+++ app/src/main/java/com/proquest/interview/phonebook/Person.kt
@@ -0,0 +1,7 @@
+package com.proquest.interview.phonebook
+
+data class Person(
+	val name: String,
+	val phoneNumber: String,
+	val address: String
+)
